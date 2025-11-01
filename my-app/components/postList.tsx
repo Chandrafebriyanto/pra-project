1201b0636068329efc7430ebdd1456015576d3d2
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-11-01 14:08:28 +0700

    commit

---
+++ my-app/components/postList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+import {Post} from "@/type/post";
+
+interface postListProps {
+  posts: Post[];
+}
+
+function postList({ posts }: postListProps): React.ReactElement {
+  return (
+    <div className="post-list">
+      {posts.map((post) => (
+        <div className="post-item" key={post.id}>
+          <h2>
+            <Link href={`/`}>{post.title}</Link>
+          </h2>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default postList;
